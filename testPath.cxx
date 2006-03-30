b9146432ddbd265afde9e60f9cadeddd9ad819da
richardb <none>
2006-03-30 05:29:50 +0200

    including changes from home
    darcs-hash:20060330032950-fafb9-ab248d2feb34712e49c62cf1ef48bb1186b4a0f9.gz

--- testPath.cxx
+++ testPath.cxx
@@ -1,8 +1,10 @@
+#include <iomanip>
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 
 #include "itkMinimalPathImageFilter.h"
 #include "itkSimpleFilterWatcher.h"
+#include "itkTimeProbe.h"
 
 int main(int, char * argv[])
 {
@@ -16,22 +18,45 @@ int main(int, char * argv[])
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
   reader->Update();
+
+  // read the marker image
+  ReaderType::Pointer reader2 = ReaderType::New();
+  reader2->SetFileName( argv[2] );
+  reader2->Update();
   
-  int height = atoi(argv[4]);
-  int F = atoi(argv[5]);
   typedef itk::MinimalPathImageFilter< IType, IType > MinPathType;
   MinPathType::Pointer path = MinPathType::New();
   path->SetInput( reader->GetOutput() );
-  
-  itk::SimpleFilterWatcher watcher(path, "path");
- 
+  path->SetMarkerImage(reader2->GetOutput());
+  //itk::SimpleFilterWatcher watcher(path, "path");
 
-  path->Update();
+  MinPathType::LabelVectorType order;
+  order.push_back(1);
+  order.push_back(2);
+  order.push_back(3);
+  order.push_back(1);
+  path->SetLabelChain(order);
+
+//  path->SetStartLabel(1);
+//  path->SetEndLabel(2);
+
+  itk::TimeProbe ltime;
+  
+  for (int i = 0;i<100;i++) 
+    {
+    ltime.Start();
+    path->Update();
+    ltime.Stop();
+    path->Modified();
+    }
+
+  std::cout << std::setprecision(3) << "Path time : " 
+            << ltime.GetMeanTime() << std::endl;
 
   typedef itk::ImageFileWriter< IType > WriterType;
   WriterType::Pointer writer = WriterType::New();
-  std::cout << "writing " << argv[2] << std::endl;
-  writer->SetFileName( argv[2] );
+  std::cout << "writing " << argv[3] << std::endl;
+  writer->SetFileName( argv[3] );
   writer->SetInput(path->GetOutput());
   writer->Update();
 
