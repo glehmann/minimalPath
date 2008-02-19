91ef9b07ec44960cbd748692fd2de491fbb1f567
richardb <none>
2008-02-19 11:25:07 +0100

    added testpath3d
    darcs-hash:20080219102507-fafb9-e13d2570dbd205d6c314fd64c27c52fcae28c88d.gz

---
+++ testPath3d.cxx
@@ -0,0 +1,104 @@
+#include <iomanip>
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+#include "itkMinimalPathImageFilter.h"
+#include "itkSimpleFilterWatcher.h"
+#include "itkTimeProbe.h"
+#include "itkLabelOverlayImageFilter.h"
+
+int main(int argc, char * argv[])
+{
+
+  const int dim = 3;
+  typedef unsigned short PType;
+  typedef itk::Image< PType, dim >    IType;
+  
+  // read the input image
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  reader->Update();
+
+  // read the marker image
+  ReaderType::Pointer reader2 = ReaderType::New();
+  reader2->SetFileName( argv[2] );
+  reader2->Update();
+  
+  typedef itk::MinimalPathImageFilter< IType, IType > MinPathType;
+  MinPathType::Pointer path = MinPathType::New();
+  path->SetInput( reader->GetOutput() );
+  path->SetMarkerImage(reader2->GetOutput());
+  //itk::SimpleFilterWatcher watcher(path, "path");
+
+  int repeats = atoi(argv[3]);
+
+  MinPathType::LabelVectorType order;
+  if (argc > 5)
+    {
+    for (int i = 5; i < argc;i++)
+      {
+      int v = atoi(argv[i]);
+      order.push_back(v);
+      }
+    } 
+  else
+    {
+    std::cout << "Using labels 1 and 2" << std::endl;
+    order.push_back(1);
+    order.push_back(2);
+    }
+  path->SetLabelChain(order);
+  path->SetDirection(2);
+
+  itk::TimeProbe ltime;
+  
+  for (int i = 0;i<repeats;i++) 
+    {
+    path->Modified();
+    ltime.Start();
+    path->Update();
+    ltime.Stop();
+    }
+
+  std::cout << std::setprecision(3) << "Path time : " 
+            << ltime.GetMeanTime() << std::endl;
+
+  const MinPathType::CostVectorType CV = path->GetCosts();
+  MinPathType::CostVectorType::const_iterator CVIt;
+  for (CVIt = CV.begin();CVIt!=CV.end();CVIt++)
+    {
+    std::cout << std::setprecision(3) << "Path cost : " << *CVIt << std::endl;
+    }
+
+#if 0
+  // set up the overlay here
+  typedef itk::RGBPixel<unsigned char> CPType;
+  typedef itk::Image< CPType, dim > RGBType;
+
+  typedef itk::LabelOverlayImageFilter<IType, IType, RGBType> OvType;
+  OvType::Pointer OvFilt = OvType::New();
+  OvFilt->SetInput(reader->GetOutput());
+  OvFilt->SetLabelImage(path->GetOutput());
+  OvFilt->SetOpacity(1);
+  typedef itk::ImageFileWriter< RGBType > RGBWriterType;
+  RGBWriterType::Pointer  RGBwriter =  RGBWriterType::New();
+
+  RGBwriter->SetInput(OvFilt->GetOutput());
+  RGBwriter->SetFileName(argv[5]);
+  RGBwriter->Update();
+
+  // save a marker image
+  OvFilt->SetLabelImage(reader2->GetOutput());
+  RGBwriter->SetFileName(argv[4]);
+  RGBwriter->Update();
+#else
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName(argv[4]);
+  writer->SetInput(path->GetOutput());
+  writer->Update();
+#endif
+  return 0;
+}
+
